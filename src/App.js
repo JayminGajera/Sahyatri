636f80661cfe4a721e80d8a42fb1f4de24e36a3c
JayminGajera <jaymingajera2332@gmail.com>
2024-01-04 23:46:39 +0530

    start screen added

--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ import OtpVerify from "./components/Auth/OtpVerify";
 import MapScreen from "./components/RiderScreen/MapScreen";
 import PrivateRoute from "./components/Auth/PrivateRoute";
 import Error from "./components/Error";
+import Start_1 from "./components/Start_1";
+import Start_2 from "./components/Start_2";
+import Start_3 from "./components/Start_3";
 
 function App() {
   return (
     <div className="min-h-screen md:h-fit h-full w-full bg-[#171515]">
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/start-1" element={<Start_1 />} />
+        <Route path="/start-2" element={<Start_2 />} />
+        <Route path="/start-3" element={<Start_3 />} />
         <Route path="/choice" element={<Choice />} />
         <Route path="/signup" element={<SignUp />} />
         <Route
--- src/components/Start.js
+++ src/components/Start.js
@@ -10,7 +10,7 @@ const Start = () => {
     <div className='flex flex-col h-screen justify-center items-center'>
       <img src={sahyatri} alt='app-logo'/>
 
-      <div onClick={() => navigate("/choice")}>
+      <div onClick={() => navigate("/start-1")}>
         <button className='bg-[#FF8000] text-white rounded-full text-[0.9rem] md:text-lg px-8 py-2 md:py-2 hover:translate-y-[1px]'>Get Started</button>
       </div>
     </div>
