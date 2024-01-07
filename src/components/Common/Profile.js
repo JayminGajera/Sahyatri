e31f71907e7b3095afa7aaade5f038991bbe879e
JayminGajera <jaymingajera2332@gmail.com>
2024-01-07 17:54:10 +0530

    .

--- src/components/Common/Profile.js
+++ src/components/Common/Profile.js
@@ -7,15 +7,15 @@ const Profile = () => {
     <div className="absolute top-12 left-0 bg-gray-800 h-[88vh] md:h-fit w-full md:w-full rounded-md">
             <div className="flex flex-col justify-center items-start p-5 gap-y-5">
               <img className="w-20 rounded-full" src={user?.image} />
-              <h1 className="text-2xl">{user.name}</h1>
-              <p ><span className="opacity-70">Account Type</span> : {user.accountType}</p>
-              <p ><span className="opacity-70">Mobile No</span> : {user.mobileNumber}</p>
-              <p ><span className="opacity-70">Email</span> : {user.email}</p>
-              <p ><span className="opacity-70">vehicle No</span> : {user.vehicleNumber}</p>
-              <p ><span className="opacity-70">Driving Licence No</span> : {user.drivingLicence}</p>
-              <p ><span className="opacity-70">Aadhar Card No</span> : {user.photoId}</p>
-              <p ><span className="opacity-70">Verify</span> : {user.verified ? "Verfied" : "Unverify"}</p>
-              <p ><span className="opacity-70">Starting Date</span> : {user.createdAt}</p>
+              <h1 className="text-2xl">{user?.name}</h1>
+              <p ><span className="opacity-70">Account Type</span> : {user?.accountType}</p>
+              <p ><span className="opacity-70">Mobile No</span> : {user?.mobileNumber}</p>
+              <p ><span className="opacity-70">Email</span> : {user?.email}</p>
+              <p >{user.accountType === "Driver" ? <span >vehicle No : <span className="opacity-70">{user?.vehicleNumber}</span></span>   : " "} </p>
+              <p >{user.accountType === "Driver" ? <span >Driving Licence No : <span className="opacity-70">{user?.drivingLicence}</span></span>   : " "} </p>
+              <p >{user.accountType === "Driver" ? <span >Aadhar Card No : <span className="opacity-70">{user?.photoId}</span></span>   : " "} </p>
+              <p ><span className="opacity-70">Verify</span> : {user?.verified ? "Verfied" : "Unverify"}</p>
+              <p ><span className="opacity-70">Starting Date</span> : {user?.createdAt}</p>
             </div>
         </div>
   )
