9a3d23babe94c280f15f99c33e85174a403f259a
JayminGajera <jaymingajera2332@gmail.com>
2024-01-07 17:55:14 +0530

    code formatted

--- src/components/Common/Profile.js
+++ src/components/Common/Profile.js
@@ -1,24 +1,62 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-    const user = useSelector((store) => store.user.user);
+  const user = useSelector((store) => store.user.user);
   return (
     <div className="absolute top-12 left-0 bg-gray-800 h-[88vh] md:h-fit w-full md:w-full rounded-md">
-            <div className="flex flex-col justify-center items-start p-5 gap-y-5">
-              <img className="w-20 rounded-full" src={user?.image} />
-              <h1 className="text-2xl">{user?.name}</h1>
-              <p ><span className="opacity-70">Account Type</span> : {user?.accountType}</p>
-              <p ><span className="opacity-70">Mobile No</span> : {user?.mobileNumber}</p>
-              <p ><span className="opacity-70">Email</span> : {user?.email}</p>
-              <p >{user.accountType === "Driver" ? <span >vehicle No : <span className="opacity-70">{user?.vehicleNumber}</span></span>   : " "} </p>
-              <p >{user.accountType === "Driver" ? <span >Driving Licence No : <span className="opacity-70">{user?.drivingLicence}</span></span>   : " "} </p>
-              <p >{user.accountType === "Driver" ? <span >Aadhar Card No : <span className="opacity-70">{user?.photoId}</span></span>   : " "} </p>
-              <p ><span className="opacity-70">Verify</span> : {user?.verified ? "Verfied" : "Unverify"}</p>
-              <p ><span className="opacity-70">Starting Date</span> : {user?.createdAt}</p>
-            </div>
-        </div>
-  )
-}
+      <div className="flex flex-col justify-center items-start p-5 gap-y-5">
+        <img className="w-20 rounded-full" src={user?.image} />
+        <h1 className="text-2xl">{user?.name}</h1>
+        <p>
+          <span className="opacity-70">Account Type</span> : {user?.accountType}
+        </p>
+        <p>
+          <span className="opacity-70">Mobile No</span> : {user?.mobileNumber}
+        </p>
+        <p>
+          <span className="opacity-70">Email</span> : {user?.email}
+        </p>
+        <p>
+          {user.accountType === "Driver" ? (
+            <span>
+              vehicle No :{" "}
+              <span className="opacity-70">{user?.vehicleNumber}</span>
+            </span>
+          ) : (
+            " "
+          )}{" "}
+        </p>
+        <p>
+          {user.accountType === "Driver" ? (
+            <span>
+              Driving Licence No :{" "}
+              <span className="opacity-70">{user?.drivingLicence}</span>
+            </span>
+          ) : (
+            " "
+          )}{" "}
+        </p>
+        <p>
+          {user.accountType === "Driver" ? (
+            <span>
+              Aadhar Card No :{" "}
+              <span className="opacity-70">{user?.photoId}</span>
+            </span>
+          ) : (
+            " "
+          )}{" "}
+        </p>
+        <p>
+          <span className="opacity-70">Verify</span> :{" "}
+          {user?.verified ? "Verfied" : "Unverify"}
+        </p>
+        <p>
+          <span className="opacity-70">Starting Date</span> : {user?.createdAt}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
